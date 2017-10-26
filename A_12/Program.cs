cdc80fb1b4b055e3b57c61dbec2dfef7f01b0a5f
Piotrek <@>
2017-10-26 12:17:42 +0200

    przesłanie plików

---
+++ A_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź ciąg znaków/zdanie.");
+            string zdanie = Console.ReadLine();
+            string[] slowa = zdanie.Split(new[] { " " }, StringSplitOptions.None);
+            string slowo = "";
+            int licznik = 0;
+            foreach (String s in slowa)
+            {
+                if (s.Length > licznik)
+                {
+                    slowo = s;
+                    licznik = s.Length;
+                }
+            }
+
+            Console.WriteLine(slowo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Napisz program, który będzie wyszukiwał najdłuższe słowo ze wprowadzonego ciągu znaków z konsoli. Wynik wyświetlony ma być na konsoli.
+
+Przykładowe wejście:
+
+sfsdf fff44JJ##$ ddfff 4343
+
+Przykładowe wyjście:
+
+fff44JJ##$
